32163d72930c748d997d5e22ce8b331d0189e68b
David Bedford <beds2004@hotmail.com>
2022-10-01 02:48:56 +0000

    Create home page to display a list of threads

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,21 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 in CodeSandbox!" />
+  <div class="container">
+    <page-home />
+  </div>
 </template>
 
 <script>
-import HelloWorldVue from "./components/HelloWorld.vue";
+import PageHome from "./components/PageHome.vue";
+
 export default {
   name: "App",
   components: {
-    HelloWorld: HelloWorldVue,
-  },
+    PageHome
+  }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "assets/styles/main.css";
 </style>
