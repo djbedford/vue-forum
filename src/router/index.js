4769558da501d60ce06111b97fa42094d29f0165
David Bedford <beds2004@hotmail.com>
2022-10-02 08:53:14 +0000

    Create component and route for 404 errors

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import PageHome from "@/components/PageHome.vue";
 import PageThreadShow from "@/components/PageThreadShow.vue";
+import PageNotFound from "@/components/PageNotFound.vue";
+
+import sourceData from "@/data.json";
 
 const routes = [
   {
@@ -13,7 +16,28 @@ const routes = [
     path: "/thread/:id",
     name: "ThreadShow",
     component: PageThreadShow,
-    props: true
+    props: true,
+    beforeEnter(to, from, next) {
+      const threadExists = sourceData.threads.find(
+        thread => thread.id === to.params.id
+      );
+
+      if (!threadExists) {
+        return next({
+          name: "NotFound",
+          params: { pathMatch: to.path.substring(1).split("/") },
+          query: to.query,
+          hash: to.hash
+        });
+      }
+
+      return next();
+    }
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "NotFound",
+    component: PageNotFound
   }
 ];
 
