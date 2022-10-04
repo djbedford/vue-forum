898e2328afc826d3e0bd705fbd10d03774acd113
David Bedford <beds2004@hotmail.com>
2022-10-04 04:47:03 +0000

    Small tweaks for better UX

--- src/router/index.js
+++ src/router/index.js
@@ -18,7 +18,11 @@ const routes = [
   {
     path: "/me",
     name: "Profile",
-    component: Profile
+    component: Profile,
+    meta: {
+      toTop: true,
+      smoothScroll: true
+    }
   },
   {
     path: "/me/edit",
@@ -71,5 +75,18 @@ const routes = [
 
 export default createRouter({
   history: createWebHistory(),
-  routes
+  routes,
+  scrollBehavior(to) {
+    const scroll = {};
+
+    if (to.meta.toTop) {
+      scroll.top = 0;
+    }
+
+    if (to.meta.smoothScroll) {
+      scroll.behavior = "smooth";
+    }
+
+    return scroll;
+  }
 });
