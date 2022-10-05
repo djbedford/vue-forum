435982b0b071771873652c29d1705aa8df9e5bd8
David Bedford <beds2004@hotmail.com>
2022-10-05 06:09:35 +0000

    Install and initialize firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apiKey: "AIzaSyBLK7HN2viJKsZmsGenvk6dM2gCbfBAhJE",
+  authDomain: "vue-forum-cfe21.firebaseapp.com",
+  projectId: "vue-forum-cfe21",
+  storageBucket: "vue-forum-cfe21.appspot.com",
+  messagingSenderId: "969044756468",
+  appId: "1:969044756468:web:848ad1f0880c04e0dcdba1"
+};
